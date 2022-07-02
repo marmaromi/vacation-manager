6ffcfc25cea69f0ead1cc77f0f1090c05d719bdf
marmaromi <marmaromi@gmail.com>
2022-07-02 18:07:00 +0300

    fixed error mesages

--- 3-middleware/catch-all.js
+++ 3-middleware/catch-all.js
@@ -52,10 +52,6 @@ function catchAll(error, req, res, next) {
                     // Log error to log file    
                 }
             }
-            else {
-                status = 500;
-                message = "Server Error";
-            }
             res.status(status).send(message);
             return [2 /*return*/];
         });
